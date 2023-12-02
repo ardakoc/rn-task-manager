d1d1cd5518b65204a7972d2941849ed2b354c125
Arda Koç <ardaskoc@gmail.com>
2023-12-03 01:50:36 +0300

    add a button to navigate to other screen

--- screens/TaskListScreen.js
+++ screens/TaskListScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import TaskList from "../components/TaskList";
 
 const styles = StyleSheet.create({
@@ -13,20 +14,48 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 16,
     },
+    addButton: {
+        position: 'absolute',
+        bottom: 24,
+        right: 24,
+        backgroundColor: '#4caf50',
+        borderRadius: 50,
+        width: 64,
+        height: 64,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 36,
+        fontWeight: 'bold',
+    },
 })
 
 const TaskListScreen = () => {
+    const navigation = useNavigation();
     const tasks = [
         { id: 1, title: 'Learn React Native' },
         { id: 2, title: 'Develop an app with React Native' },
     ]
 
+    const handleAddTask = (newTask) => {
+        // TODO: implement this function.
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Task List</Text>
             <TaskList tasks={tasks} />
+            <TouchableOpacity 
+                style={styles.addButton}
+                onPress={() =>
+                    navigation.navigate('Add Task', { onAddTask: handleAddTask })
+                }
+            >
+                <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
-}
+}  
 
 export default TaskListScreen;
