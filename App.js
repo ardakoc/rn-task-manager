4aa3a29dcd6f5a701cb9cd5cfa9753709c9d0640
Arda Koç <ardaskoc@gmail.com>
2023-12-03 01:51:45 +0300

    add navigation container to the app

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TaskListScreen from './screens/TaskListScreen';
+import TaskAddScreen from './screens/TaskAddScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TaskListScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TaskList">
+        <Stack.Screen name="My Tasks" component={TaskListScreen} />
+        <Stack.Screen name="Add Task" component={TaskAddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
