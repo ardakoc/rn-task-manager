1052a1a8fda847af211a188152e4e7143a41be63
Arda Koç <ardaskoc@gmail.com>
2023-12-03 01:49:22 +0300

    add task-add screen

---
+++ screens/TaskAddScreen.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray', 
+        borderWidth: 1,
+        marginBottom: 16,
+        paddingHorizontal: 8,
+    },
+})
+
+const TaskAddScreen = ({ navigation }) => {
+    const [taskTitle, setTaskTitle] = useState('')
+
+    const handleAddTask = () => {
+        // TODO: Add task function.
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Task title"
+                value={taskTitle}
+                onChangeText={(text) => setTaskTitle(text)}
+            />
+            <Button title="Submit" onPress={handleAddTask} />
+        </SafeAreaView>
+    )
+}
+
+export default TaskAddScreen;
