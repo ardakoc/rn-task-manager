dbd0cf227f1163e32a6c7d6594c581b0ff78f23f
Arda Koç <ardaskoc@gmail.com>
2023-12-06 16:05:52 +0300

    add details input to task add screen. update styling.

--- screens/TaskAddScreen.js
+++ screens/TaskAddScreen.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
-import { Button, SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 const TaskAddScreen = ({ route, navigation }) => {
     const { onAddTask, tasks } = route.params
     const [taskTitle, setTaskTitle] = useState('')
+    const [taskDetails, setTaskDetails] = useState('')
 
     const handleAddTask = () => {
-        const newTask = { id: tasks.length + 1, title: taskTitle}
+        const newTask = { title: taskTitle, details: taskDetails }
         onAddTask(newTask)
         navigation.goBack()
     }
 
     return (
         <SafeAreaView style={styles.container}>
-            <TextInput 
-                style={styles.input}
-                placeholder="Task title"
-                value={taskTitle}
-                onChangeText={(text) => setTaskTitle(text)}
-            />
+            <View style={styles.formElement}>
+                <Text style={styles.label}>Title:</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Task title"
+                    value={taskTitle}
+                    onChangeText={(text) => setTaskTitle(text)}
+                />
+            </View>
+            <View style={styles.formElement}>
+                <Text style={styles.label}>Details:</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Task details"
+                    value={taskDetails}
+                    onChangeText={(text) => setTaskDetails(text)}
+                />
+            </View>
             <Button title="Submit" onPress={handleAddTask} />
         </SafeAreaView>
     )
@@ -30,17 +43,20 @@ const styles = StyleSheet.create({
         padding: 16,
         backgroundColor: '#fff',
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
+    formElement: {
+        marginHorizontal: 24,
+        marginTop: 16,
+    },
+    label: {
+        marginTop: 8,
+        marginBottom: 4,
     },
     input: {
         height: 40,
-        borderColor: 'gray', 
+        borderColor: '#777',
+        color: '#777',
         borderWidth: 1,
-        marginBottom: 16,
-        paddingHorizontal: 8,
+        paddingHorizontal: 16,
     },
 })
 
